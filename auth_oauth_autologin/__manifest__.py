ec2fed73dca9372ffdc8f7fd392d09487bd4394d
wluyima <wyclif@openmrs.org>
2025-07-04 15:30:40 +0300

    Moved all auto login  implementation to the backend

--- auth_oauth_autologin/__manifest__.py
+++ auth_oauth_autologin/__manifest__.py
@@ -12,6 +12,5 @@
     "website": "https://github.com/OCA/server-auth",
     "depends": ["auth_oauth"],
     "data": ["views/auth_oauth_provider.xml"],
-    "demo": [],
-    "assets": {"web.assets_frontend": ["auth_oauth_autologin/static/src/js/*.js"]},
+    "demo": []
 }
--- auth_oauth_autologin/controllers/main.py
+++ auth_oauth_autologin/controllers/main.py
@@ -20,15 +20,17 @@ class OAuthAutoLogin(OAuthLogin):
         if len(providers) == 1:
             return providers[0].get("auth_link")
 
-    @http.route(
-        "/auth/auto_login_redirect_link",
-        type="json",
-        auth="none",
-    )
-    def auto_login_redirect_link(self, *args, **kwargs):
-        redirect = kwargs.get("redirect")
-        if self._autologin_disabled(redirect):
+    def auto_login_link(self):
+        if self._autologin_disabled(request.httprequest.url):
             return False
-        request.params["redirect"] = redirect
-        auth_link = self._autologin_link()
-        return auth_link
+        return self._autologin_link()
+
+    @http.route("/web/login", type="http", auth="none")
+    def web_login(self, *args, **kw):
+        if not request.session.uid:
+            if not request.params or not request.params.get("oauth_error"):
+                auth_link = self.auto_login_link()
+                if auth_link:
+                    return request.redirect(auth_link, local=False)
+
+        return super().web_login(*args, **kw)
