5b703a057b1eaf4ae9a0f337116efd1ffa9d5aca
wluyima <wyclif@openmrs.org>
2025-07-09 16:55:19 +0300

    Added missing @http.route

--- auth_oauth_autologin/controllers/main.py
+++ auth_oauth_autologin/controllers/main.py
@@ -3,6 +3,7 @@
 
 from urllib.parse import parse_qsl, urlparse
 
+from odoo import http
 from odoo.http import request
 
 from odoo.addons.auth_oauth.controllers.main import OAuthLogin
@@ -19,6 +20,7 @@ class OAuthAutoLogin(OAuthLogin):
         if len(providers) == 1:
             return providers[0].get("auth_link")
 
+    @http.route("/web/login", type="http", auth="none")
     def web_login(self, *args, **kw):
         if not request.session.uid:
             if not self._autologin_disabled(request.httprequest.url):
