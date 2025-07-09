410bed8bfad8c44b0c385b74ea62d4597847c1bd
wluyima <wyclif@openmrs.org>
2025-07-09 16:38:20 +0300

    Added tests for autologin feature

--- auth_oauth_autologin/controllers/main.py
+++ auth_oauth_autologin/controllers/main.py
@@ -3,15 +3,14 @@
 
 from urllib.parse import parse_qsl, urlparse
 
-from odoo import http
 from odoo.http import request
 
 from odoo.addons.auth_oauth.controllers.main import OAuthLogin
 
 
 class OAuthAutoLogin(OAuthLogin):
-    def _autologin_disabled(self, redirect):
-        url = urlparse(redirect)
+    def _autologin_disabled(self, request_url):
+        url = urlparse(request_url)
         params = dict(parse_qsl(url.query, keep_blank_values=True))
         return "no_autologin" in params or "oauth_error" in params or "error" in params
 
@@ -20,7 +19,6 @@ class OAuthAutoLogin(OAuthLogin):
         if len(providers) == 1:
             return providers[0].get("auth_link")
 
-    @http.route("/web/login", type="http", auth="none")
     def web_login(self, *args, **kw):
         if not request.session.uid:
             if not self._autologin_disabled(request.httprequest.url):
